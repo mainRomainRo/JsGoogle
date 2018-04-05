412b4436d6f8bc3e2a59a1f3b8d06f25d9edc7df
ZCLMK <zacharie.NTC@gmail.com>
2018-04-05 15:24:01 +0200

    moteur de recherche

--- JsGoogle.js
+++ JsGoogle.js
@@ -1,11 +1,31 @@
 // 1.Création du bouton magique
 
-var overCenter = document.getElementById('toggleCenter')
-overCenter.addEventListener('click',(e) => {
-  var centerDiv = document.getElementById("search")
-  centerDiv.classList.remove("hidden")
-  overCenter.style.display = "none"
-  console.log(centerDiv)
+  var overCenter = document.getElementById('toggleCenter')
+  overCenter.addEventListener('click',(e) => {
+
+    var centerDiv = document.getElementById("search")
+    centerDiv.classList.remove("hidden")
+    overCenter.style.display = "none"
+    console.log(centerDiv)
 })
 
 // 2.
+// La barre
+var monInput = document.getElementById("input")
+// le bouton
+var rechercher = document.getElementById("recherche")
+
+
+rechercher.onclick = function(){
+
+  var saisie = monInput.value
+  var monUrl = "https://www.google.fr/#q="
+  var maRecherche = monUrl + saisie
+  console.log(saisie)
+  if (saisie !== ""){
+      window.location.replace(maRecherche)
+  }
+
+
+  //
+}
